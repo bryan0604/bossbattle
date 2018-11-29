948ddee07af1b9f92babdf8d586fd506b806d42d
Anduin Lothar <brisseis0604@gmail.com>
2018-11-29 15:16:10 +0800

    atom ge
    Co-Authored-By: null <bryan0604@users.noreply.github.com>

--- Boss Battle Multiplayer/Assets/NewBehaviourScript.cs
+++ Boss Battle Multiplayer/Assets/NewBehaviourScript.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
-
+public class NewBehaviourScript : MonoBehaviour
+{
+	float A;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		if(A <= 0)
+		{
+			Debug.Log("A");
+		}
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+
 	}
 }
